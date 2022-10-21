13f585dc621eba3a416e6481c6c10f5b9071b915
Matt Lim <mlimcollege@gmail.com>
2022-10-21 00:03:21 -0400

    Export parsing methods

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
 import FlipperSdk from "sdk/FlipperSdk";
 
+import parseCreateBettorInfoIx from "parse/parseCreateBettorInfoIx";
+import parseFlipIx from "parse/parseFlipIx";
+import parsePayoutIx from "parse/parsePayoutIx";
+import parsePlaceBetIx from "parse/parsePlaceBetIx";
+
+export { parseCreateBettorInfoIx, parseFlipIx, parsePayoutIx, parsePlaceBetIx };
+
 export default FlipperSdk;
