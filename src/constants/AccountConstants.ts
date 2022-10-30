9ace338bbc5a12bcfc2e498f63e88a3e0fe4064c
Matt Lim <mlimcollege@gmail.com>
2022-10-30 16:57:46 -0700

    Add AUTHORITY pubkey for mainnet

--- src/constants/AccountConstants.ts
+++ src/constants/AccountConstants.ts
@@ -8,10 +8,13 @@ export const WRAPPED_SOL_MINT = new PublicKey(
 const AUTHORITY_DEV = new PublicKey(
   "authyWritV3BSybcBdkQc1fMaQ7ycidrvYiRKvtjbec"
 );
+const AUTHORITY_MAINNET = new PublicKey(
+  "auth6FQ5A3YM8YR77w8XzrCaTk4sSnUGAcNmPVtHMZT.json"
+);
+
 export const AUTHORITIES = {
   [Environment.Development]: AUTHORITY_DEV,
   [Environment.Local]: AUTHORITY_DEV,
   [Environment.Testnet]: AUTHORITY_DEV,
-  // TODO: fill in with real authority
-  [Environment.Production]: AUTHORITY_DEV,
+  [Environment.Production]: AUTHORITY_MAINNET,
 };
